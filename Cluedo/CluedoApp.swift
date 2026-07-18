2ffd03a43b7fae69486ee0859a3394aa12d92e80
Nicole Kong <mningk@gmail.com>
2026-07-17 23:45:19 -0700

    Initial Commit

---
+++ Cluedo/CluedoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CluedoApp.swift
+//  Cluedo
+//
+//  Created by Nicole Kong Mei Ning on 17/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct CluedoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
